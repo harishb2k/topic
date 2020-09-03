946fd7d4757ae6e278ab47af50c68d730da6e006
Harish Singh Bohara <harish.bohara@olacabs.com>
2020-09-03 14:31:49 +0530

    fixed command for flink

--- flink/flink_lyft/setup.sh
+++ flink/flink_lyft/setup.sh
@@ -7,9 +7,9 @@ kubectl create -f https://raw.githubusercontent.com/lyft/flinkk8soperator/v0.5.0
 
 kubectl create -f https://raw.githubusercontent.com/lyft/flinkk8soperator/v0.5.0/deploy/config.yaml
 
-https://raw.githubusercontent.com/harishb2k/topic/master/flink/flink_lyft/flinkk8soperator.yaml
+kubectl create -f https://raw.githubusercontent.com/harishb2k/topic/master/flink/flink_lyft/flinkk8soperator.yaml
 kubectl get pods -n flink-operator
 
-https://raw.githubusercontent.com/harishb2k/topic/master/flink/flink_lyft/flink-operator-custom-resource.yaml
+kubectl create -f https://raw.githubusercontent.com/harishb2k/topic/master/flink/flink_lyft/flink-operator-custom-resource.yaml
 kubectl get deployments -n flink-operator
 kubectl get flinkapplication.flink.k8s.io -n flink-operator wordcount-operator-example -o yaml
